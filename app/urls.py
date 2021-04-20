b1f507163da934f0df9ae5bd39cce99f1d44a7d3
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-04-20 11:00:52 +0100

    first commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import home, about, contact
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about")
+]
